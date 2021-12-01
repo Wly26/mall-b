96ef4d7bd46dbb67817811f2e5dddc3ac28d0168
Wly26 <8431716+wly26@user.noreply.gitee.com>
2021-12-01 14:24:08 +0800

    start

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="tab-bar-item" @click="linkclick">
+    <div class="item-icon" v-show="!isActive">
+      <slot name="icon">
+      </slot>
+    </div>
+    <div class="active-icon" v-show="isActive">
+      <slot name="active-icon">
+      </slot>
+    </div>
+    <div class="item-text" :style="activeStyle">
+      <slot name="item-text">
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props:{
+    link:{
+      type: String,
+    }
+  },
+  data() {
+    return{
+    }
+  },
+  computed:{
+    isActive(){
+      return this.$route.path.indexOf(this.link) !== -1
+    },
+    activeStyle(){
+      return this.isActive ? {'color': 'red'} : {}
+    }
+  },
+  methods:{
+    linkclick(){
+      this.$router.push(this.link)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .tab-bar-item{
+    flex: 1;
+    .item-icon img{
+      width: 24px;
+      height: 24px;
+      margin-top: 5px;
+      vertical-align: middle;
+    }
+    .active-icon img{
+      width: 24px;
+      height: 24px;
+      margin-top: 5px;
+      vertical-align: middle;
+    }
+    .item-text{
+      font-size: 12px;
+      margin-top: 3px;
+      color: #333;
+    }
+  }
+</style>
---
+++ src/components/content/mainTabBar/mainTabBar.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="test6">
+    <p>6.tabbar</p>
+    <hr>
+    <router-view></router-view>
+    <tabbar>
+      <tab-bar-item link="/Home">
+        <template #icon>
+          <img src="~assets/img/tabbar/home.svg">
+        </template>
+        <template #active-icon>
+          <img src="~assets/img/tabbar/home_active.svg">
+        </template>
+        <template #item-text>
+          <div>首页</div>
+        </template>
+      </tab-bar-item>
+
+      <tab-bar-item link="/category">
+        <template #icon>
+          <img src="~assets/img/tabbar/cart.svg">
+        </template>
+        <template #active-icon>
+          <img src="~assets/img/tabbar/cart_active.svg">
+        </template>
+        <template #item-text>
+          <div>分类</div>
+        </template>
+      </tab-bar-item>
+
+      <tab-bar-item link="/category">
+        <template #icon>
+          <img src="~assets/img/tabbar/category.svg">
+        </template>
+        <template #active-icon>
+          <img src="~assets/img/tabbar/category_active.svg">
+        </template>
+        <template #item-text>
+          <div>分类</div>
+        </template>
+      </tab-bar-item>
+      
+      <tab-bar-item link="/profile">
+        <template #icon>
+          <img src="~assets/img/tabbar/profile.svg">
+        </template>
+        <template #active-icon>
+          <img src="~assets/img/tabbar/profile_active.svg">
+        </template>
+        <template #item-text>
+          <div>我的</div>
+        </template>
+      </tab-bar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import Tabbar from 'common/tabbar/tabbar.vue'
+import TabBarItem from 'common/tabbar/TabBarItem.vue'
+export default {
+  components: { TabBarItem, Tabbar },
+  name: 'maintabbar',
+  data() {
+    return{
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  img{
+    flex: 1;
+    width: 24px;
+    height: 24px;
+    margin-top: 5px;
+    vertical-align: middle;
+  }
+</style>
